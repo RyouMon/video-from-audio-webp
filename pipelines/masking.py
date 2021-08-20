a884b14a1eef6433c82f3e6a1ef95efbbe943245
Wen <wenslife@outlook.com>
2021-08-20 23:28:57 +0800

    refactor: remove audio stream from MaskingPipeline

--- pipelines/masking.py
+++ pipelines/masking.py
@@ -9,4 +9,4 @@ class MaskingPipeline(Pipeline):
         mask = ffmpeg.input('color=black:1080x600', f='lavfi')
         video = video.overlay(mask, shortest=1)
         video = video.overlay(mask, y='H-600', shortest=1)
-        return self.output(video, context['a'], outfile), context
+        return self.output(video, outfile), context
