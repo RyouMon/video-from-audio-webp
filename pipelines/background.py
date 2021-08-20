931a8e2cc666db0208c157824a9f08bdb7387da2
Wen <wenslife@outlook.com>
2021-08-20 13:01:58 +0800

    refactor(pipeline): using lavfi in BlackBackgroundPipeline

--- pipelines/background.py
+++ pipelines/background.py
@@ -7,5 +7,5 @@ class BlackBackgroundPipeline(Pipeline):
     def process(self, infile, outfile, context):
         input_a = self.input(infile).audio
         context = {'a': input_a}
-        input_v = ffmpeg.input('fixtures/black1080x1920.jpg', loop=1)
+        input_v = ffmpeg.input('color=black:1080x1920', f='lavfi')
         return self.output(input_v, input_a, outfile, y=None, acodec='aac', r=25, shortest=None), context
