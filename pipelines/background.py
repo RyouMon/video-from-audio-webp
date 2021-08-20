29ab54d119af1dba9fa6b5976cd9a62c02282fcf
Wen <wenslife@outlook.com>
2021-08-20 22:42:13 +0800

    fix: incorrect updating context

--- pipelines/background.py
+++ pipelines/background.py
@@ -6,6 +6,6 @@ class BlackBackgroundPipeline(Pipeline):
 
     def process(self, infile, outfile, context):
         input_a = self.input(infile).audio
-        context = {'a': input_a}
+        context.update({'a': input_a})
         input_v = ffmpeg.input('color=black:1080x1920', f='lavfi')
         return self.output(input_v, input_a, outfile, acodec='aac', shortest=None), context
