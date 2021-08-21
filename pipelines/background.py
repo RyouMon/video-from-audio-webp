6ae2bc17e41c6c38e3742c3ff06a376a29702465
Wen <wenslife@outlook.com>
2021-08-21 10:04:48 +0800

    refactor: rename BlackBackgroundPipeline to SocialMediaBlackBackgroundPipeline

--- pipelines/background.py
+++ pipelines/background.py
@@ -2,10 +2,21 @@ import ffmpeg
 from pipelines import Pipeline
 
 
-class BlackBackgroundPipeline(Pipeline):
+class SocialMediaBlackBackgroundPipeline(Pipeline):
 
     def process(self, infile, outfile, context):
-        input_a = self.input(infile).audio
-        context.update({'a': input_a})
-        input_v = ffmpeg.input('color=black:1080x1920', f='lavfi')
-        return self.output(input_v, input_a, outfile, acodec='aac', shortest=None), context
+        duration = context.get('duration', 0)
+        self.validate_duration(duration)
+
+        stream = ffmpeg.input('color=black:1080x1920', f='lavfi')
+        return self.output(stream, outfile, t=duration), context
+
+    def validate_duration(self, value):
+
+        if not value:
+            self.logger.error('output duration set to 0!')
+            raise ValueError('You must set duration(>0) on context!')
+
+        if not isinstance(value, (int, float)):
+            self.logger.error(f'output duration set to {value!r}!')
+            raise ValueError('duration must a int or float!')
