b908c41f7f6a9a8492aa631ac76fdd7a361447d2
Wen <wenslife@outlook.com>
2021-08-21 10:28:04 +0800

    refactor: SocialMediaBlackBackgroundPipeline can accept a str duration

--- pipelines/background.py
+++ pipelines/background.py
@@ -6,17 +6,22 @@ class SocialMediaBlackBackgroundPipeline(Pipeline):
 
     def process(self, infile, outfile, context):
         duration = context.get('duration', 0)
-        self.validate_duration(duration)
+        duration = self.clean_duration(duration)
 
         stream = ffmpeg.input('color=black:1080x1920', f='lavfi')
         return self.output(stream, outfile, t=duration), context
 
-    def validate_duration(self, value):
+    def clean_duration(self, value):
 
         if not value:
             self.logger.error('output duration set to 0!')
             raise ValueError('You must set duration(>0) on context!')
 
-        if not isinstance(value, (int, float)):
+        if isinstance(value, (int, float)):
+            return value
+
+        try:
+            return float(value)
+        except ValueError:
             self.logger.error(f'output duration set to {value!r}!')
             raise ValueError('duration must a int or float!')
