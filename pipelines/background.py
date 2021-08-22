0128132f498c05492dda15edf25c0d606d8add28
Wen <wenslife@outlook.com>
2021-08-22 14:43:30 +0800

    refactor: simplify SocialMediaBlackBackgroundPipeline

--- pipelines/background.py
+++ pipelines/background.py
@@ -5,23 +5,7 @@ from pipelines import Pipeline
 class SocialMediaBlackBackgroundPipeline(Pipeline):
 
     def process(self, infile, outfile, context):
-        duration = context.get('duration', 0)
-        duration = self.clean_duration(duration)
+        audio = self.input(infile).audio
 
         stream = ffmpeg.input('color=black:1080x1920', f='lavfi')
-        return self.output(stream, outfile, t=duration), context
-
-    def clean_duration(self, value):
-
-        if not value:
-            self.logger.error('output duration set to 0!')
-            raise ValueError('You must set duration(>0) on context!')
-
-        if isinstance(value, (int, float)):
-            return value
-
-        try:
-            return float(value)
-        except ValueError:
-            self.logger.error(f'output duration set to {value!r}!')
-            raise ValueError('duration must a int or float!')
+        return self.output(stream, audio, outfile, shortest=None), context
