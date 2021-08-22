00412d6ffbbaded4175f8130eac1464e39c7dd49
Wen <wenslife@outlook.com>
2021-08-22 14:52:57 +0800

    refactor: add audio steam to ContinuousTextPipeline and SubtitlePipeline

--- pipelines/text.py
+++ pipelines/text.py
@@ -5,16 +5,19 @@ class ContinuousTextPipeline(Pipeline):
 
     def process(self, infile, outfile, context):
         video = self.input(infile)
+        audio = video.audio
 
         for text, font, size, color, x, y in context['texts']:
             video = video.drawtext(text=text, fontfile=font, fontcolor=color, fontsize=size, x=x, y=y)
 
-        return self.output(video, outfile), context
+        return self.output(video, audio, outfile), context
 
 
 class SubtitlePipeline(Pipeline):
 
     def process(self, infile, outfile, context):
         video = self.input(infile)
+        audio = video.audio
+
         video = video.filter('subtitles', context['srt_file'], force_style=context.get('force_style'))
-        return self.output(video, outfile), context
+        return self.output(video, audio, outfile), context
